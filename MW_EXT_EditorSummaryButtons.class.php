fb410f2e5a6f06f2035301bb62101f983b6a2974
Kitsune Solar <kitsune.solar@gmail.com>
2018-09-23 03:34:55 +0300

    2018-09-23 00:34:55

---
+++ MW_EXT_EditorSummaryButtons.class.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace MediaWiki\Extension\MetaStore;
+
+use OutputPage, Skin;
+
+/**
+ * Class MW_EXT_Key
+ */
+class MW_EXT_EditorSummaryButtons {
+
+	/**
+	 * Load resource function.
+	 *
+	 * @param OutputPage $out
+	 * @param Skin $skin
+	 *
+	 * @return bool
+	 */
+	public static function onBeforePageDisplay( OutputPage $out, Skin $skin ) {
+		$out->addModules( [ 'ext.mw.editor-summary-button' ] );
+		$out->addModuleStyles( [ 'ext.mw.editor-summary-button.styles' ] );
+
+		return true;
+	}
+}
